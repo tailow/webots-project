00c3feafb57f5c458007a79e76c987b9650aaa0e
tailow <tailows@outlook.com>
2022-12-10 23:47:46 +0200

    Added object avoidance using sensor data

--- controllers/object_avoider_controller/object_avoider_controller.py
+++ controllers/object_avoider_controller/object_avoider_controller.py
@@ -7,6 +7,8 @@ robot = Robot()
 # get the time step of the current world.
 timestep = 64
 
+MAX_SPEED = 6.28
+
 right_motor = robot.getDevice("motor_1")
 left_motor = robot.getDevice("motor_2")
 
@@ -21,9 +23,22 @@ front_right_sensor.enable(1)
 right_motor.setPosition(float('inf'))
 left_motor.setPosition(float('inf'))
 
-right_motor.setVelocity(1.0)
-left_motor.setVelocity(1.0)
+right_motor.setVelocity(MAX_SPEED)
+left_motor.setVelocity(MAX_SPEED)
 
 # Main loop:
 while robot.step(timestep) != -1:
-    pass
+    fl_value = front_left_sensor.getValue() / 1000
+    fr_value = front_right_sensor.getValue() / 1000
+    f_value = front_sensor.getValue() / 1000
+
+    if f_value < 1 or fl_value < 1 or fr_value < 1:
+        if fr_value < fl_value:
+            right_motor.setVelocity(-MAX_SPEED / 2)
+            left_motor.setVelocity(MAX_SPEED / 2)
+        else:
+            right_motor.setVelocity(MAX_SPEED / 2)
+            left_motor.setVelocity(-MAX_SPEED / 2)
+    else:
+        right_motor.setVelocity(MAX_SPEED)
+        left_motor.setVelocity(MAX_SPEED)
