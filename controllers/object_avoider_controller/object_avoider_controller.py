4f7a6a42b17bd3348bc760270c4bf83aa90c69db
tailow <tailows@outlook.com>
2022-12-11 02:38:17 +0200

    Added more sensors to the robot

--- controllers/object_avoider_controller/object_avoider_controller.py
+++ controllers/object_avoider_controller/object_avoider_controller.py
@@ -8,6 +8,7 @@ robot = Robot()
 timestep = 64
 
 MAX_SPEED = 6.28
+MIN_DIST = 0.3
 
 right_motor = robot.getDevice("motor_1")
 left_motor = robot.getDevice("motor_2")
@@ -15,10 +16,16 @@ left_motor = robot.getDevice("motor_2")
 front_sensor = robot.getDevice("ds1")
 front_left_sensor = robot.getDevice("ds2")
 front_right_sensor = robot.getDevice("ds3")
+down_sensor = robot.getDevice("ds4")
+left_sensor = robot.getDevice("ds5")
+right_sensor = robot.getDevice("ds6")
 
 front_sensor.enable(1)
 front_left_sensor.enable(1)
 front_right_sensor.enable(1)
+down_sensor.enable(1)
+left_sensor.enable(1)
+right_sensor.enable(1)
 
 right_motor.setPosition(float('inf'))
 left_motor.setPosition(float('inf'))
@@ -27,12 +34,12 @@ right_motor.setVelocity(MAX_SPEED)
 left_motor.setVelocity(MAX_SPEED)
 
 def turn_left():
-    right_motor.setVelocity(MAX_SPEED / 2)
-    left_motor.setVelocity(-MAX_SPEED / 2)
-   
-def turn_right():
     right_motor.setVelocity(-MAX_SPEED / 2)
     left_motor.setVelocity(MAX_SPEED / 2)
+   
+def turn_right():
+    right_motor.setVelocity(MAX_SPEED / 2)
+    left_motor.setVelocity(-MAX_SPEED / 2)
     
 def go_straight():
     right_motor.setVelocity(MAX_SPEED)
@@ -43,13 +50,17 @@ while robot.step(timestep) != -1:
     fl_value = front_left_sensor.getValue() / 1000
     fr_value = front_right_sensor.getValue() / 1000
     f_value = front_sensor.getValue() / 1000
-
+    d_value = down_sensor.getValue() / 1000
+    l_value = left_sensor.getValue() / 1000
+    r_value = right_sensor.getValue() / 1000
+    
+    sensor_values = [fl_value, fr_value, f_value, l_value, r_value]
+  
     # obstacle avoidance
-    if f_value < 1 or fl_value < 1 or fr_value < 1:
-        if fr_value < fl_value:
+    for value in sensor_values:
+        if value < MIN_DIST or d_value > 0.35:
             turn_right()
-        else:
-            turn_left()
+            break
             
     # drive straight
     else:
